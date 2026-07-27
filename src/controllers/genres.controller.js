42bcadbf5ec233cc96b93d56d767d7f5c1edbd5c
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-07-27 21:21:19 +0200

    added endpoint to delete genres whether is not been used for other objects

--- src/controllers/genres.controller.js
+++ src/controllers/genres.controller.js
@@ -1,6 +1,14 @@
 import Genre from "../models/genre.model.js";
-import { InsertError, ValidationError } from "../../shared/errors/app.error.js";
+import Song from "../models/song.model.js";
+import Album from "../models/album.model.js";
+import {
+  InsertError,
+  ValidationError,
+  AppError,
+  NotFoundError,
+} from "../../shared/errors/app.error.js";
 import { withoutBody } from "../../utils/validations.js";
+
 /* Add new genres to db */
 const insertGenre = async (req, res, next) => {
   try {
@@ -23,5 +31,32 @@ const insertGenre = async (req, res, next) => {
     next(new InsertError(`Error: Failed insert new genre -> ${error}`));
   }
 };
+/* Delete genre */
+const deleteGenre = async (req, res, next) => {
+  try {
+    const genre = await Genre.findById(req.params.id);
+    if (!genre)
+      return next(
+        new NotFoundError("This genre hasn't been found in the database"),
+      );
 
-export { insertGenre };
+    const [songCounts, albumCounts] = await Promise.all([
+      Song.countDocuments({ genre: genre._id }),
+      Album.countDocuments({ genre: genre._id }),
+    ]);
+
+    if (songCounts > 0 || albumCounts > 0)
+      return next(
+        new ValidationError(
+          "Denied opertion: This genre is been used on several songs or albums.",
+        ),
+      );
+
+    const deleted = await Genre.findByIdAndDelete(genre._id);
+    res.status(200).json({ deleted });
+  } catch (error) {
+    next(new AppError(`Inexpected error deleting genre -> ${error}`));
+  }
+};
+
+export { insertGenre, deleteGenre };
--- src/routes/genres.routes.js
+++ src/routes/genres.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { insertGenre } from "../controllers/genres.controller.js";
+import { insertGenre, deleteGenre } from "../controllers/genres.controller.js";
 import isAuth from "../../shared/middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.post("/create",isAuth("admin"), insertGenre);
+router.post("/create", isAuth("admin"), insertGenre);
+router.delete("/:id", isAuth("admin"), deleteGenre);
 
 export default router;
